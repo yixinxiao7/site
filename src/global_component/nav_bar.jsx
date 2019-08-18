75842d2267e4497a9728ce0f1642d67c980a6575
Yixin Xiao <yixinx@umich.edu>
2019-08-18 19:41:47 -0400

    fixed header and texts

--- src/global_component/nav_bar.jsx
+++ src/global_component/nav_bar.jsx
@@ -6,14 +6,14 @@ import '../pages/css/global_comp.css';
 export const renderNavBar = () =>{
     return(
         <Navbar className = "navbar-custom" expand="lg">
-            <Navbar.Brand href="/"><p className = "light-blue header">Yixin Xiao</p></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto ">
-                    <NavLink tag={Link} to="/aboutMe"><p className = "white normal-text mx-3">About Me</p></NavLink>
-                    <NavLink tag={Link} to="/portfolio"><p className = "white normal-text mx-3">Porfolio</p></NavLink>
-                    <NavLink tag={Link} to="/research"><p className = "white normal-text mx-3">Research</p></NavLink>
-                    <NavLink tag={Link} to="/contactMe"><p className = "white normal-text mx-3">Contact Me</p></NavLink>
+                <Nav className="mr-auto">
+                    <NavLink tag={Link} to="/"><h1 className="light-blue header mx-3">Yixin Xiao</h1></NavLink>
+                    <NavLink tag={Link} to="/aboutMe"><p className = "white normal-text pt-3 mx-3">About Me</p></NavLink>
+                    <NavLink tag={Link} to="/portfolio"><p className = "white py-auto normal-text pt-3 mx-3">Porfolio</p></NavLink>
+                    <NavLink tag={Link} to="/research"><p className = "white py-auto normal-text pt-3 mx-3">Research</p></NavLink>
+                    <NavLink tag={Link} to="/contactMe"><p className = "white py-auto normal-text pt-3 mx-3">Contact Me</p></NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
