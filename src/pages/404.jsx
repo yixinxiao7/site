7521111336966f04442ad78379fec7e8b97d6829
Yixin Xiao <yixinx@umich.edu>
2019-08-28 16:59:21 -0400

    added surprise

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
-
+import DOGGO from '../images/CORGI.jpg';
+import './css/404.css';
 const NotFound = () => {
     return (
-        <div>
+        <div className="color">
             <h3> 404: NOT FOUND</h3>
+            <p className="bigger-text">but here's a cute picture of a corgi :)</p>
+            <img src={DOGGO} className="image-resize"/>
         </div>
     );
 }
