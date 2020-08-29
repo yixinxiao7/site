a698f06b54dd69a14a39fdaadbdc64511516fd05
Yixin Xiao <yixinxiao7@gmail.com>
2020-08-29 17:02:26 -0400

    changed color scheme to match

--- src/global_component/nav_bar.jsx
+++ src/global_component/nav_bar.jsx
@@ -10,10 +10,10 @@ export const renderNavBar = () =>{
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <NavLink tag={Link} to="/"><h1 className="light-green header mx-3">Yixin Xiao</h1></NavLink>
-                    <NavLink tag={Link} to="/aboutMe"><p className = "white normal-text pt-3 mx-3">About Me</p></NavLink>
-                    <NavLink tag={Link} to="/portfolio"><p className = "white py-auto normal-text pt-3 mx-3">Portfolio</p></NavLink>
-                    <NavLink tag={Link} to="/research"><p className = "white py-auto normal-text pt-3 mx-3">Research</p></NavLink>
-                    <NavLink tag={Link} to="/contactMe"><p className = "white py-auto normal-text pt-3 mx-3">Contact Me</p></NavLink>
+                    <NavLink tag={Link} to="/aboutMe"><p className = "light-grey normal-text pt-3 mx-3">About Me</p></NavLink>
+                    <NavLink tag={Link} to="/portfolio"><p className = "light-grey py-auto normal-text pt-3 mx-3">Portfolio</p></NavLink>
+                    <NavLink tag={Link} to="/research"><p className = "light-grey py-auto normal-text pt-3 mx-3">Research</p></NavLink>
+                    <NavLink tag={Link} to="/contactMe"><p className = "light-grey py-auto normal-text pt-3 mx-3">Contact Me</p></NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/pages/about_me.jsx
+++ src/pages/about_me.jsx
@@ -17,7 +17,7 @@ function Phone() {
   
     return (
       <>
-      <i class="fa fa-phone" Style="font-size:36px; padding-right:2%;" ref={target} onClick={() => setShow(!show)}></i>
+      <i class="fa fa-phone" Style="font-size:36px; padding-right:2%; color:#c0dfd0" ref={target} onClick={() => setShow(!show)}></i>
         <Overlay target={target.current} show={show} placement="right">
           {(props) => (
             <Tooltip id="overlay-example" {...props}>
@@ -33,7 +33,7 @@ const renderIntro = () =>{
     return(
         <div className="slight-right">
             <h2>
-                Here's me!
+                Here's <span className="dark-green">me</span>!
             </h2>
             <ul>
                 <li>Motivated: I intrinsically seek to learn more and better myself.</li>
@@ -42,16 +42,16 @@ const renderIntro = () =>{
                 <li>Maryland-based: So as one may expect, I love crabs. But I'm all open for relocating!</li>
                 <li>Corgi-lover: Don't get me wrong. I love almost all animals. But corgis are EXTRA cute.</li>
             </ul>
-            <h3>Check out my <a href={Resume} target="_blank">resume</a>! If you're interested, feel free to contact me.</h3>
+            <h3>Check out my <a href={Resume} target="_blank" className="link-color">resume</a>! If you're interested, feel free to contact me.</h3>
             <div>
-                <a href="https://github.com/yixinxiao7" target="_blank">
+                <a href="https://github.com/yixinxiao7" target="_blank" className="link-color">
                     <i class="fa fa-github" Style="font-size:36px; padding-right:2%;"/>
                 </a>
-                <a href="https://www.linkedin.com/in/yixin-xiao-4851a2163/" target="_blank">
+                <a href="https://www.linkedin.com/in/yixin-xiao-4851a2163/" target="_blank" className="link-color">
                     <i class="fa fa-linkedin-square" aria-hidden="true" Style="font-size:36px; padding-right:2%;"/>
                 </a>
                 <Phone />
-                <a href="mailto:yixinx@umich.edu" target="_blank">
+                <a href="mailto:yixinx@umich.edu" target="_blank" className="link-color">
                     <i class="fa fa-envelope" Style="font-size:36px; padding-right:2%;"/>
                 </a>
                 
@@ -85,51 +85,48 @@ const renderWorkExp=()=>{
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="June 2020 - August 2020"
-            iconStyle={{ background: '#45A29E', color: '#0B0C10' }}
-            icon={<FaRobot />}
+            iconStyle={{ background: '#d3d7db'}}
         >
-            <h3 className="vertical-timeline-element-title db-text">Capital One</h3>
-            <h4 className="vertical-timeline-element-subtitle db-text">Software Engineering Intern</h4>
-            <hr className="line-db"></hr>
-            <p >
+            <h3 className="vertical-timeline-element-title">Capital One</h3>
+            <h4 className="vertical-timeline-element-subtitle">Software Engineering Intern</h4>
+            <hr className="line-color"></hr>
+            <p className="bg-grey">
                 At Capital One I worked on a business-facing application focused on automating fraud compliance processes. The application helps users not only assign compliance tasks/issues to sofware teams via JIRA (Atlassian's agile-focused, work-management tool), but also provides detailed and visual analytics about ongoing compliance tasks.
             </p>
-            <p>
+            <p className="bg-grey">
                 Utilizing Java + Spring Boot, I developed a RESTful API that interacts with both external (JIRA) and internal Capital One services. Users can create/edit JIRA epics and assign them to dynamically-updated teams and retrieve parsed information about ongoing fraud tasks in real time. I leveraged AngularJS to develop a corresponding UI that allows users to interact with the backend; examples include a form that they can send over to the API to create an epic as well as a grid that displays data such as current progress on the epics and their corresponding stories.
             </p>
-            <p>
+            <p className="bg-grey">
                 To boost the security of the application, I implemented SSO that features single-use tokens and user logging to limit users to Capital One employees. Furthermore, I used Capital One's key management system to prevent project secrets from leaking out of the code source. 
             </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="May 2019 - August 2019"
-            iconStyle={{ background: '#45A29E', color: '#0B0C10' }}
-            icon={<FaGlasses />}
+            iconStyle={{ background: '#d3d7db'}}
         >
-            <h3 className="vertical-timeline-element-title db-text">Applied Research Associates</h3>
-            <h4 className="vertical-timeline-element-subtitle db-text">Scientific Programming Software Engineering Intern</h4>
-            <hr className="line-db"></hr>
-            <p>
+            <h3 className="vertical-timeline-element-title">Applied Research Associates</h3>
+            <h4 className="vertical-timeline-element-subtitle">Scientific Programming Software Engineering Intern</h4>
+            <hr className="line-color"></hr>
+            <p className="bg-grey">
                 At ARA, I worked on refactoring a legacy simulation program with over 100,000 LOC and 25 years of development. This was an interesting challenge as the interdiscliplinary project involved both software and scientific concepts and practices.
             </p>
-            <p>
+            <p className="bg-grey">
                 Throughout the summer I worked on merging two separate code databases, and constructing a <a href="https://en.wikipedia.org/wiki/Dynamic-link_library" target="_blank">Dynamic-Link Library</a> that would allow different driver programs to use specific components of functions. Using Fortran (yes, Fortran), I developed my first API that maintained the functionality of the existing GUI for the program and allowed users to run simulations with unique parameters. To make data manipulation/interaction easier on users, I revamped the I/O system such that data would be stored in memory rather than outputted to text files, allowing users to retrieve data in-program directly after a simulation run.
             </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="June 2018 - August 2018"
-            iconStyle={{ background: '#45A29E', color: '#0B0C10' }}
-            icon={<FaGlasses />}
+            iconStyle={{ background: '#d3d7db'}}
         >
-            <h3 className="vertical-timeline-element-title db-text">Posterity Group</h3>
-            <h4 className="vertical-timeline-element-subtitle db-text">Project Management / IT Intern</h4>
-            <hr className="line-db"></hr>
-            <p>
+            <h3 className="vertical-timeline-element-title">Posterity Group</h3>
+            <h4 className="vertical-timeline-element-subtitle">Project Management / IT Intern</h4>
+            <hr className="line-color"></hr>
+            <p className="bg-grey">
                 During this internship, I had the chance to work both on PM and IT tasks. With the business undergoing a transformative change in its hiring process, I was able to play a key role in expediting this by documenting the current process, identifying issues and developing goals to remediate those. These were done through a series of meetings involving the CEO, COO and other project managers, where I also aplied Poka Yoke and other Six Sigma concepts to promote quality assurance for future steps.
             </p>
-            <p>
+            <p className="bg-grey">
                 On the IT side, I used tools such as Javascript and Microsoft Flow to automate and accelerate office workflows, produce more efficient interdepartmental work cycles and further promote QA.
             </p>
         </VerticalTimelineElement>
@@ -141,35 +138,34 @@ const renderResearchExp=()=>{
         <VerticalTimeline layout="1-column">
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2018 - present"
-            iconStyle={{ background: '#45A29E', color: '#0B0C10' }}
-            icon={<FaRobot />}
+            date="September 2018 - February 2020"
+            iconStyle={{ background: '#84a9a1'}}
         >
-            <h3 className="vertical-timeline-element-title db-text">Crowds and Machines Lab</h3>
-            <h4 className="vertical-timeline-element-subtitle db-text">Undergraduate Research Assistant</h4>
-            <hr className="line-db"></hr>
-            <p >
-            
+            <h3 className="vertical-timeline-element-title">Crowds and Machines Lab</h3>
+            <h4 className="vertical-timeline-element-subtitle">Undergraduate Research Assistant</h4>
+            <hr className="line-color"></hr>
+            <p className="bg-grey">
             From 2018-2019, my research centered around machine learning with a focus on active learning and deep learning. The focus of my project was to make a meta learner to select the best active leaning strategy to train a machine learning model depending on its training stage, in order to achieve a fast-learning and accurate model. 
-            <br/><br/>
+            </p>
+            <p className="bg-gre">
             I developed a flexible wrapper class for both custom and Pytorch convolution neural networks, with an API similar to that found on scikit-learn models. This wrapper class was used to develop the experimental pipeline, building part of the foundation of the research project.
-
-            <br/><br/>
+            </p>
+            <p className="bg-grey">
             Additionally, I worked on expanding the current datasets being used for the experiments,by adding Twenty News (text dataset). I leveraged the raw text data anyone can retrieve from scikit-learn, and converted it into an adapted, inerhited class from our main data loader class, following the structure of the class hierarchy. This will allow for other machine learning problems, such as Natural Language Processing, to be tested with the metalearner.   
             </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2017 - 2018"
-            iconStyle={{ background: '#45A29E', color: '#0B0C10' }}
-            icon={<FaGlasses />}
+            date="September 2017 - April 2018"
+            iconStyle={{ background: '#84a9a1'}}
         >
-            <h3 className="vertical-timeline-element-title db-text">University of Michigan</h3>
-            <h4 className="vertical-timeline-element-subtitle db-text">Undergraduate Research Assistant</h4>
-            <hr className="line-db"></hr>
-            <p>
+            <h3 className="vertical-timeline-element-title">University of Michigan</h3>
+            <h4 className="vertical-timeline-element-subtitle">Undergraduate Research Assistant</h4>
+            <hr className="line-color"></hr>
+            <p className="bg-grey">
             From 2017-2018, I conducted research on Bluetooth technology and augmented reality (AR). During the first half of the year, the goal was to enable Bluetooth functionality into everyday goods. To test this, I worked with an arduino to remotely light up the adafruit neopixel (led lights).
-            <br/><br/>
+            </p>
+            <p className="bg-grey">
             During the second half, I worked with augmented reality to test its impact on the domestic manufacturing process and industrial design. Due to the very general and exploratory nature of the research topic, I focused on setting up on the foundation of the project— I created an augmented reality application using apple’s ARKit, which allows users place and remove and .obj file onto any detected planes. This will help with architectural analysis and can be easily extended or modified to perform other tasks.
             </p>
         </VerticalTimelineElement>
