c9640b2e0ece5e17d0afd9bc8ceb5fc36f05543c
Yixin Xiao <yixinxiao7@gmail.com>
2020-08-30 14:59:42 -0400

    added animations to main page

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -4,22 +4,33 @@ import Pic from '../images/YX.jpg';
 import './css/home_page.css'
 import './css/global_comp.css'
 import {renderNavBar} from '../global_component/nav_bar.jsx';
+import styled, { keyframes } from 'styled-components';
+import { fadeIn } from 'react-animations'
+
+
+const fadeInAnimation = keyframes`${fadeIn}`;
+
+const FadeInDiv = styled.div`
+  animation: 2s ${fadeInAnimation};
+`;
 
 const renderJumbotron = () =>{
     return(
-        <Jumbotron align="center" className = "bg-trans">
-            <div className="vcenter">
-                <h1 className="large-font">
-                    Hello, I'm <span className="light-green">Yixin</span>!
-                </h1>   
-                <h3>
-                    I'm a passionate <span className="dark-green">student</span>
-                    , driven <span className="dark-green">software engineer</span>
-                    , and avid <span className="dark-green"> corgi-lover</span>.
-                    </h3>
-                <Button variant="outline-light" href="#/aboutMe" className="mt-2">More about me</Button>
-            </div>
-        </Jumbotron>
+        <FadeInDiv>
+            <Jumbotron align="center" className = "bg-trans">
+                <div className="vcenter">
+                    <h1 className="large-font">
+                        Hello, I'm <span className="light-green">Yixin</span>!
+                    </h1>   
+                    <h3>
+                        I'm a passionate <span className="dark-green">student</span>
+                        , driven <span className="dark-green">software engineer</span>
+                        , and avid <span className="dark-green"> corgi-lover</span>.
+                        </h3>
+                    <Button variant="outline-light" href="#/aboutMe" className="mt-2">More about me</Button>
+                </div>
+            </Jumbotron>
+        </FadeInDiv>
     );
 }
 
